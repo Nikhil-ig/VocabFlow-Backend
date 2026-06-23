101aa5eda490c4c4e6ee809062f5cc49f81e7c2f
Nikhil-ig <nikhil.nn696@gmail.com>
2026-06-23 09:02:24 +0530

    fix(cors): set origin to true to reflect request origin unconditionally

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@ const port = process.env.PORT || 4000;
 export const prisma = new PrismaClient();
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL ? [process.env.FRONTEND_URL, 'http://localhost:3000'] : 'http://localhost:3000',
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
